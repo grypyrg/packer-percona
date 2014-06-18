38a7013d3fb55f46c59cb0ad01b75151d33f0430
Jay Janssen <jay.janssen@percona.com>
2014-06-18 09:30:30 -0400

    mend

---
+++ provisioners/prepare-udev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash		
+
+# Ensure udev doesn't mess with the network
+echo "Ensuring udev doesn't mess with the network..."
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+sed -i 's/^HWADDR.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^UUID.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
+
