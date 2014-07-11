5c133e05ab4672ab4d5d90dffd3d21d9d293eed3
Jay Janssen <jay.janssen@percona.com>
2014-07-11 16:29:45 -0400

    had to find a new ami to base off of that forced ec2-user

--- provisioners/prepare-ec2-ssh-key.sh
+++ provisioners/prepare-ec2-ssh-key.sh
@@ -2,3 +2,9 @@
 
 echo 'Removing root authorized_keys so EC2 will auto-populate it'
 rm -f /root/.ssh/authorized_keys
+
+
+if [ -f /etc/cloud/cloud.cfg ]; then
+  echo 'Allowing root login via cloud-init'
+  sed -i "s/^disable_root: 1/disable_root: 0/" /etc/cloud/cloud.cfg
+fi
