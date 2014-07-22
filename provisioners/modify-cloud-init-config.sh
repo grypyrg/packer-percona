75dd3f047c6c21c47b4328463f62af1c7fb617a4
Jay Janssen <jay.janssen@percona.com>
2014-07-22 09:57:06 -0400

    enable password authentication

--- provisioners/modify-cloud-init-config.sh
+++ provisioners/modify-cloud-init-config.sh
@@ -4,6 +4,9 @@ if [ -f /etc/cloud/cloud.cfg ]; then
   echo 'Allowing root login via cloud-init'
   sed -i "s/^disable_root: 1/disable_root: 0/" /etc/cloud/cloud.cfg
 
+  echo 'Allowing password logins'
+  sed -i "s/^ssh_pwauth:   0/ssh_pwauth:   1/" /etc/cloud/cloud.cfg
+
   echo 'Skip mounting extra storage'
   sed -i "/^ - mounts/d" /etc/cloud/cloud.cfg
 fi
