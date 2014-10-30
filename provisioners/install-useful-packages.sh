b94f1ad8b460e38a5ec4b51a6ba45c177eef1b25
Jay Janssen <jay.janssen@percona.com>
2014-10-30 14:39:25 -0400

    Move dkms and htop to provisioner list
    Guarantees epel access

--- provisioners/install-useful-packages.sh
+++ provisioners/install-useful-packages.sh
@@ -4,7 +4,7 @@ echo "Installing useful packages"
 
 if [ -x /usr/bin/yum ]; then
 	yum localinstall -y http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
-	yum install -y puppet screen telnet unzip lsof ntp ntpdate wget sysstat bind-utils
+	yum install -y puppet screen telnet unzip lsof ntp ntpdate wget sysstat bind-utils dkms htop
 
 elif [ -x /usr/bin/apt-get ]; then
 	apt-get install puppet screen telnet unzip lsof ntp ntpdate wget sysstat bind-utils -y
