174b58f5aae0ec44bb7af32d3b0d6e3803995e3b
Kenny Gryp <kenny.gryp@percona.com>
2016-02-15 12:25:10 +0100

    cloud based vms should not have vagrant insecure key at all, centos 6 cloud-init support was added and the old rc.local type is now removed in the init scripts, removed openstack completely for now

--- provisioners/cloud.sh
+++ provisioners/cloud.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+
+#----------------------------------------------
+# the 32bit amazon AMI instance is built from 
+# http://www.nixknight.com/2014/04/build-ebs-backed-centos-ec2-ami-from-scratch/
+# 
+# However, we don't want to use it anymore. i want to use cloud-init to setup 
+# my accounts. so we need to remove the /etc/rc.local hack.
+# Making a new AMI is time consuming so this will work too :-)
+#
+
+if [ "3376ff19b847690778b317b9500a2178" == "`md5sum /etc/rc.d/rc.local | awk '{print $1}'`" ]; then
+	cat > /etc/rc.d/rc.local << EOF
+#!/bin/sh
+#
+# This script will be executed *after* all the other init scripts.
+# You can put your own initialization stuff in here if you don't
+# want to do the full Sys V style init stuff.
+ 
+touch /var/lock/subsys/local
+EOF
+fi
+
+
 #----------------------------------------------
 if [ $PACKER_BUILDER_TYPE == "amazon-ebs" ]
 then
@@ -9,14 +32,23 @@ then
 fi
 
 #----------------------------------------------
+# This is for centos7 which uses cloud-init
 if [ -f /etc/cloud/cloud.cfg ]; then
 	echo "Configure cloud-init"
 
-	sed -i "s/^disable_root: 1$/disable_root: 0/" /etc/cloud/cloud.cfg
-	sed -i "s/^ssh_pwauth:   0$/ssh_pwauth:   1/" /etc/cloud/cloud.cfg
+# - we don't want root@ssh
+# - we don't want ssh pw authentication
+#	sed -i "s/^disable_root: 1$/disable_root: 0/" /etc/cloud/cloud.cfg
+#	sed -i "s/^ssh_pwauth:   0$/ssh_pwauth:   1/" /etc/cloud/cloud.cfg
+	sed -i "s/^ssh_deletekeys:   0$/ssh_deletekeys:   1/" /etc/cloud/cloud.cfg
 	sed -i "s/^    name: centos$/    name: vagrant/" /etc/cloud/cloud.cfg
 	sed -i "/^ - mounts"/d /etc/cloud/cloud.cfg
 fi
+
+
+#----------------------------------------------
+# remove root authorized keys after finish
+rm -f /root/.ssh/authorized_keys
 
 
 sync && sleep 1 && sync
--- provisioners/vm.sh
+++ provisioners/vm.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+# Some of these things are things cloudinit would do
+
+#----------------------------------------------
+# we do not longer want root to be used
+#echo 'Install vagrant SSH key'
+#mkdir -pm 700 /root/.ssh
+#wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /root/.ssh/authorized_keys
+#chmod 0600 /root/.ssh/authorized_keys
+#chown -R root:root /root/.ssh
+# we do not longer want password auth, it's insecure
+#echo vagrant | passwd --stdin root
+
+
+
+#----------------------------------------------
+echo 'Install vagrant user'
+adduser vagrant
+# we do not longer want password auth, it's insecure
+# echo vagrant | passwd --stdin vagrant
+
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/vagrant
+
 
 #----------------------------------------------
 echo "Installing useful VM packages"
